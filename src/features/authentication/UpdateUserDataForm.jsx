4006efa18bfd9edac7ee7557d699a0c134c769d5
Gulherme Kalnin <gfkalnin@gmail.com>
2023-10-14 09:04:12 -0300

    - Implemented update of user data and password

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,27 +1,46 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import Button from "../../ui/Button";
-import FileInput from "../../ui/FileInput";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+import Button from '../../ui/Button';
+import FileInput from '../../ui/FileInput';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
 
-import { useUser } from "./useUser";
+import { useUser } from './useUser';
+import { useUpdateUser } from './useUpdateUser';
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
   const {
     user: {
       email,
-      user_metadata: { fullName: currentFullName },
+      user_metadata: { full_name: currentFullName },
     },
   } = useUser();
 
-  const [fullName, setFullName] = useState(currentFullName);
+  const { updateUser, isUpdating } = useUpdateUser();
+
+  const [full_name, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(full_name);
+    if (!full_name) return;
+    updateUser(
+      { full_name, avatar },
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset();
+        },
+      }
+    );
+  }
+
+  function handleCancel() {
+    setFullName(currentFullName);
+    setAvatar(null);
   }
 
   return (
@@ -29,26 +48,35 @@ function UpdateUserDataForm() {
       <FormRow label="Email address">
         <Input value={email} disabled />
       </FormRow>
+
       <FormRow label="Full name">
         <Input
           type="text"
-          value={fullName}
+          value={full_name}
           onChange={(e) => setFullName(e.target.value)}
           id="fullName"
+          disabled={isUpdating}
         />
       </FormRow>
+
       <FormRow label="Avatar image">
         <FileInput
           id="avatar"
           accept="image/*"
           onChange={(e) => setAvatar(e.target.files[0])}
+          disabled={isUpdating}
         />
       </FormRow>
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button
+          type="reset"
+          $variation="secondary"
+          disabled={isUpdating}
+          onClick={handleCancel}
+        >
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button disabled={isUpdating}>Update account</Button>
       </FormRow>
     </Form>
   );
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function signup({ full_name, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -42,4 +42,37 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, full_name, avatar }) {
+  // 1) Update password or full_name
+  let updateData;
+  if (password) updateData = { password };
+  if (full_name) updateData = { data: { full_name } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // 2) Upload the avatar image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from('avatars')
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // 3) Update avatar in the user
+  const { data: updatedUser, error: errorOnUpdate } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+
+  if (errorOnUpdate) throw new Error(errorOnUpdate.message);
+
+  return updatedUser;
 }
